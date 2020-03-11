16865f6d2d9f7346ecf50450a8eebca8275fe56f
keeperererer <2249269086@qq.com>
2020-03-11 17:03:17 +0800

    fix admin

--- tripServer/routes/back.js
+++ tripServer/routes/back.js
@@ -98,4 +98,198 @@ router.get('/userInfo',function(req,res,next) {
     res.json(data)
   })
 })
+router.post('/updateUserInfo',function(req,res,next) {
+  let name = req.body.name,
+      userName = req.body.userName,
+      userId = req.body.userId;
+  // res.json(name)
+  pool.query(`UPDATE user_info SET name="${name}",userName="${userName}" WHERE userId="${userId}"`,function(err,results,fields){
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200;
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
+router.post('/deleteUserInfo',function(req,res,next){
+  let userId = req.body.userId;
+  pool.query(`DELETE FROM user_info WHERE userId="${userId}"`,function(err,results,fields){
+    if(err){
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)    
+  })
+})
+router.post('/addUserInfo',function(req,res,next) {
+  let name = req.body.name,
+      userName = req.body.userName,
+      userId = req.body.userId;
+  // res.json(name)
+  pool.query(`INSERT INTO user_info SET name="${name}",userId="${userId}",userName="${userName}"`,function(err,results,fields){
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200;
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
+router.get('/totalData',function(req,res,next) {
+  pool.query(`SELECT * FROM total_data`,function(err,results,fields) {
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    data.data = results
+    res.statusCode = 200;
+    data.count = data.data.length;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
+router.post('/updateTotalData',function(req,res,next) {
+  let userId = req.body.userId,
+      allWalk = req.body.allWalk,
+      allRun = req.body.allRun,
+      allCycle = req.body.allCycle,
+      allDrive = req.body.allDrive,
+      allBus = req.body.allBus,
+      allTaxi = req.body.allTaxi;
+  // res.json(name)
+  pool.query(`UPDATE total_data SET allWalk="${allWalk}",allRun="${allRun}",allCycle="${allCycle}",allDrive="${allDrive}",allBus="${allBus}",allTaxi="${allTaxi}" WHERE userId="${userId}"`,function(err,results,fields){
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200;
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
+router.post('/deleteTotalData',function(req,res,next){
+  let userId = req.body.userId;
+  pool.query(`DELETE FROM total_data WHERE userId="${userId}"`,function(err,results,fields){
+    if(err){
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)    
+  })
+})
+router.post('/addTotalData',function(req,res,next) {
+  let userId = req.body.userId,
+      allWalk = req.body.allWalk,
+      allRun = req.body.allRun,
+      allCycle = req.body.allCycle,
+      allDrive = req.body.allDrive,
+      allBus = req.body.allBus,
+      allTaxi = req.body.allTaxi;
+  // res.json(name)
+  pool.query(`INSERT INTO total_data SET userId="${userId}",allWalk="${allWalk}",allRun="${allRun}",allCycle="${allCycle}",allDrive="${allDrive}",allBus="${allBus}",allTaxi="${allTaxi}"`,function(err,results,fields){
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200;
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
+router.get('/user',function(req,res,next) {
+  pool.query(`SELECT * FROM user`,function(err,results,fields) {
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    data.data = results
+    res.statusCode = 200;
+    data.count = data.data.length;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
+router.post('/updateUser',function(req,res,next) {
+  let passWord = req.body.passWord,
+      userName = req.body.userName,
+      userId = req.body.userId;
+  // res.json(name)
+  pool.query(`UPDATE user SET userName="${userName}",passWord="${passWord}" WHERE userId="${userId}"`,function(err,results,fields){
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200;
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
+router.post('/deleteUser',function(req,res,next){
+  let userId = req.body.userId;
+  pool.query(`DELETE FROM user WHERE userId="${userId}"`,function(err,results,fields){
+    if(err){
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)    
+  })
+})
+router.post('/addUser',function(req,res,next) {
+  let passWord = req.body.passWord,
+      userName = req.body.userName,
+      userId = req.body.userId;
+  // res.json(name)
+  pool.query(`INSERT INTO user SET userId="${userId}",userName="${userName}",passWord="${passWord}"`,function(err,results,fields){
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    res.statusCode = 200;
+    data.code = 400;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
+router.get('/tripData',function(req,res,next) {
+  pool.query(`SELECT * FROM trip_data`,function(err,results,fields) {
+    if(err) {
+      data.code = 500
+      data.msg = err
+    };
+    data.data = results
+    res.statusCode = 200;
+    data.count = data.data.length;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data)
+  })
+})
 module.exports = router;
